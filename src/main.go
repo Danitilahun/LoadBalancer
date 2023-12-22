5f7685ee6727f655794c3c18521a0e346c175c3c
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-22 14:35:35 +0300

    add a function that parse url and create new server

--- src/main.go
+++ src/main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"net/http/httputil"
+	"net/url"
 )
 
 type simpleServer struct {
 	addr  string
 	proxy *httputil.ReverseProxy
+}
+
+func newSimpleServer(addr string) *simpleServer {
+	serverUrl, err := url.Parse(addr)
+	if err != nil {
+		fmt.Println("url parse error", err)
+		panic(err)
+	}
+	return &simpleServer{
+		addr:  addr,
+		proxy: httputil.NewSingleHostReverseProxy(serverUrl),
+	}
 }
